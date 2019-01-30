0e1ef41fe6f26866d0bd72302b7546a788f62d03
saichinnu <venkatasai.573@gmail.com>
2019-01-30 12:37:31 +0530

    Updates

---
+++ precache-manifest.8f97496251efab7b862365389edafae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactonclick/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f1c9cedbbf6f71ed36a",
+    "url": "/reactonclick/static/js/runtime~main.7f1c9ced.js"
+  },
+  {
+    "revision": "c7748a977afc3c899e61",
+    "url": "/reactonclick/static/js/main.c7748a97.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/reactonclick/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "c7748a977afc3c899e61",
+    "url": "/reactonclick/static/css/main.1e6be6bd.chunk.css"
+  },
+  {
+    "revision": "319bdcd057448fd5a478f530730a9cbf",
+    "url": "/reactonclick/index.html"
+  }
+];
